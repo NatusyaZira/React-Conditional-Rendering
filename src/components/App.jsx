204f3d17e7d6510f89586831d5fb98df1ea1c7bb
Nataliia Kovalova <natusya.zira@gmail.com>
2023-03-04 18:21:48 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Form from "./Form";
+import Login from "./Login"
+
+let userIsRegistered = true;
+
+function App(){
+  return(
+    <div className="container">
+    <Form isRegistered={userIsRegistered} />
+    </div>
+  );
+}
+
+// let isLoggedIn = false;
+
+// const currentTime = new Date().getHours();
+// console.log(currentTime);
+// function App() {
+//   return <div className="container">{ 
+//     // isLoggedIn ? <h1>Hello</h1> : <Login /> 
+
+//     currentTime > 12 && <h1>Why are you still working? </h1> 
+//   }</div>;
+// }
+
+export default App;
